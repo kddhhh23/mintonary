5822177459405e9bde4d6521823d11b5f7f55f70
kddhhh23 <kddhhh23@gmail.com>
2026-07-16 16:36:08 +0900

    chore: 프로젝트 초기 세팅 (Flutter + Spring Boot)

---
+++ server/src/main/java/com/example/mintonary/MintonaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mintonary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintonaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintonaryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/mintonary/MintonaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mintonary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintonaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
